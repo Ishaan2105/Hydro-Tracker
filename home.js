7c68e90bca5c2b2e82da7146f3d06b9e27e9e488
Ishaan2105 <ishaanhingway@gmail.com>
2026-08-04 12:23:55 +0530

    chore: push all latest changes and CSS/JS updates to github repository

--- home.js
+++ home.js
@@ -1043,7 +1043,7 @@ class WaterWaves {
 
 // Initialize with your settings
 new WaterWaves('waves-bg', {
-    lineColor: "rgba(56, 189, 248, 0.45)",
+    lineColor: "#ffffff",
     waveSpeedX: 0.0125,
     waveSpeedY: 0.01,
     waveAmpX: 40,
